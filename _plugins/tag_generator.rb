cdaabd9b3589c10b12479a2f57b061490e3cae02
Bret Hudson <bret@brethudson.com>
2024-05-02 23:48:56 -0700

    Set up tags

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,24 @@
+Jekyll::Hooks.register :posts, :post_write do |post|
+	all_existing_tags = Dir.entries("_tag")
+	.map { |t| t.match(/(.*).md/) }
+	.compact.map { |m| m[1] }
+	
+	tags = post['tags'].reject { |t| t.empty? }
+	.map { |t| (
+		t.downcase
+			.gsub(' ', '-')
+			.gsub('c++', 'cpp')
+			.gsub('c#', 'c-sharp')
+	) }
+	
+	tags.each do |tag|
+		generate_tag_file(tag) if !all_existing_tags.include?(tag)
+	end
+end
+
+def generate_tag_file(tag)
+	# generate tag file
+	File.open("_tag/#{tag}.md", "wb") do |file|
+		file << "---\nlayout: tags\ntag-name: #{tag}\n---\n"
+	end
+end
